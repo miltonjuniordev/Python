9aaa94da7077540f98ec1acbf655545e902d6cd3
miltonjunior.dev <m.paz2708@gmail.com>
2024-10-29 20:51:42 -0300

    Removendo comentários do arquivo main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ def alerta (nivel, base, local):
     dataAtual = str(datetime.now())
 
     if nivel == 1: notification.notify(
-        title = f'',
-        message = f'',
+        title = f'ALERT: A base {base} está offline.',
+        message = f'Urgente verificar o servidor {local}, a base {base} está offline. \n {dataAtual}',
         app_name = 'notification',
         timeout = 10
     )
     elif nivel == 2: notification.notify(
-        title = f'',
-        message = f'',
+        title = f'ALERT: A base {base} está offline.',
+        message = f'Urgente verificar o servidor {local}, a base {base} está offline. \n {dataAtual}',
         app_name = 'notification',
         timeout = 10
     )
@@ -21,10 +21,6 @@ def alerta (nivel, base, local):
         message = f'Urgente verificar o servidor {local}, a base {base} está offline. \n {dataAtual}',
         app_name = 'notification',
         timeout = 10
-    ) 
-    #else(
-    #    print(f'{base} ou {local} não identificado.')
-    #)
+    )
 
 alerta(nivel = 3, base = 'USUÁRIO', local = './LOCALHOST')
-#print(dataAtual)
